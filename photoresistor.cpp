2952397cbfdbc6b04ab7224cc6a9d7cc8154b03e
afridimatin <77028704+afridimatin@users.noreply.github.com>
2021-04-15 04:21:50 -0600

    Update photoresistor.cpp

--- photoresistor.cpp
+++ photoresistor.cpp
@@ -1,13 +1,18 @@
 #include "C1.cpp"
+int white_led_Value = 0; // value of output
+int white_Input = 19; // value of when light is on
+int photoResistor = 0;
 
 void setup() {  
 Serial.begin(9600);
+pinMode(8, OUTPUT); 
 }
 
 void loop() {  
-int value = analogRead(A0);  
-  
-Serial.println("Analog Value :");
-Serial.println(value);  
+int value = analogRead(photoResistor);  
+Serial.println(white_led_Value);  
 delay(250);
+if (white_led_Value < white_Input) // if sensor value is less than 19, light will turn on.
+{ digitalWrite(8, HIGH);//LED on } else { digitalWrite(8, LOW);// LED off }
+}
 }
