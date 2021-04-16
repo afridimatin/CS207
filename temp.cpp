d579996bcc05bddc9b3546d6683f661513e1fee3
afridimatin <77028704+afridimatin@users.noreply.github.com>
2021-04-15 18:03:52 -0600

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,10 +1,11 @@
-//temp.cpp
-//This program controls the temp sensor & alarm.
+// This program controls the temp sensor & alarm.
+// A1 will be used for Temperature sensor.
+// PIN 2 will be used for speaker.
 
 int value_zero = 0;
 int temp_pin = A1;
 int temp_cov = 40;
-
+int spk_pin = 2;
 void setup() {
 Serial.begin(9600);
 pinMode(13, OUTPUT); 
@@ -22,7 +23,9 @@ do{
   delay(50);
   digitalWrite(13, HIGH);
   delay(100);
-  
+  tone(spk_pin, 2000, 70);
+  delay(100);
+  tone(spk_pin, 1000, 70);
       }while(temp_value > temp_cov);
       digitalWrite(13, LOW);
 } 
